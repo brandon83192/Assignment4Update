eeb1d5a8b25fb2f19869170a37a6b9b372125ad1
Btond <Btond@LAPTOP-K8U30UIB>
2020-04-25 19:17:23 -0400

    Add project files.

---
+++ IEXTrading/Controllers/HomeController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GHI.Infrastructure.GHIListHandler;
+using GHI.Models;
+//using GHI.Models.ViewModel;
+using GHI.DataAccess;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Http;
+
+namespace MVCTemplate.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        public ApplicationDbContext dbContext;
+        //private readonly AppSettings _appSettings;
+        public const string SessionKeyName = "HospitalData";
+        
+        public HomeController(ApplicationDbContext context /*IOptions<AppSettings>*/ /*appSettings*/)
+        {
+            dbContext = context;
+            //_appSettings = appSettings.Value;
+        }
+
+        //public IActionResult HelloIndex()
+        //{
+        //    ViewBag.Hello = _appSettings.Hello;
+        //    return View();
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+      
+        public IActionResult About_us()
+        {
+            return View();
+        }
+        public IActionResult Summary()
+        {
+
+
+            return View();
+
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form(Hospital hospital)
+        {
+            var details = dbContext.HospitalInfo.Where(c => c.city == hospital.city);
+
+            return View("Hospitals" , details);
+        }
+
+        public IActionResult Hospitals()
+        {
+
+            //Set ViewBag variable first
+            ViewBag.dbSucessComp = 0;
+            GHIHandler webHandler = new GHIHandler();
+            List<Hospital> hos_centers = webHandler.GetFloridadata();
+
+            String recdata = JsonConvert.SerializeObject(hos_centers);
+           
+
+            HttpContext.Session.SetString(SessionKeyName, recdata);
+
+            return View(hos_centers);
+        }
+
+
+        //public async Task<IActionResult> Index(string searchString)
+        //{
+        //    var data = from m in context.HospitalInfo
+        //               select m;
+
+        //    if (!String.IsNullOrEmpty(searchString))
+        //    {
+        //        hospital_name = hospital_name.Where(s => s.Title.Contains(searchString));
+        //    }
+
+        //    return View(await movies.ToListAsync());
+        //}
+
+        public IActionResult savedata()
+        {
+            string hospitalsData = HttpContext.Session.GetString(SessionKeyName);
+            List<Hospital> hospitals = null;
+            if (hospitalsData != "")
+            {
+                hospitals = JsonConvert.DeserializeObject<List<Hospital>>(hospitalsData);
+            }
+
+            foreach (Hospital hospital in hospitals)
+            {
+                //   Database will give PK constraint violation error when trying to insert record with existing PK.
+                //  So add company only if it doesnt exist, check existence using symbol (PK)
+                // if (dbContext.Companies.Where(c => c.symbol.Equals(company.symbol)).Count() == 0)
+                // {
+                dbContext.HospitalInfo.Add(hospital);
+                // }
+                // }
+                dbContext.SaveChanges();
+                ViewBag.dbSuccessComp = 1;
+
+            }
+            return View("Hospitals", hospitals);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        
+
+    }
+}
---
+++ IEXTrading/Infrastructure/GHIListHandler/GHIHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using GHI.Models;
+using Newtonsoft.Json;
+
+namespace GHI.Infrastructure.GHIListHandler
+{
+    public class GHIHandler
+    {
+        static string BASE_URL = "https://data.medicare.gov/resource/xubh-q36u.json"; //This is the base URL, method specific URL is appended to this.
+        HttpClient httpClient;
+
+        public GHIHandler()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+       
+
+        public List<Hospital> GetFloridadata()
+        {
+            string GHIHospital_API_PATH = BASE_URL + "?$where=state='FL'";
+            string companyList = "";
+
+            List<Hospital> companies = null;
+
+            httpClient.BaseAddress = new Uri(GHIHospital_API_PATH);
+            HttpResponseMessage response = httpClient.GetAsync(GHIHospital_API_PATH).GetAwaiter().GetResult();
+            if (response.IsSuccessStatusCode)
+            {
+                companyList = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            }
+
+            if (!companyList.Equals(""))
+            {
+                companies = JsonConvert.DeserializeObject<List<Hospital>>(companyList);
+                companies = companies.GetRange(0, 50);
+            }
+            return companies;
+        }
+        
+        
+    }
+}
